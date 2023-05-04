94ac5d3e68a2536f33b216260bd0b3eae1606bf9
sangwoo_kim <kimz1121@naver.com>
2023-05-04 18:44:58 +0900

    0504 ai test 03

--- lunar_lander/library_test 2.py
+++ lunar_lander/library_test 2.py
@@ -2,8 +2,14 @@ import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
 
+import time
 
-env = gym.make("LunarLander-v2", render_mode="human")
+
+from keras.models       import Sequential
+from keras.layers       import Dense
+from keras.optimizers   import Adam
+
+# env = gym.make("LunarLander-v2", render_mode="human")
 # env = gym.make("LunarLander-v2", render_mode="rgb_array") 
 # # WSL2 에서 pygame 화면 출력이 안되던 문제는, anaconda 가상환경의 화면 출력 라이브러리 관련 문재 였음. 
 # 현재는 가상환경 사용을 배제 하거나, 가상환경용 라이브러리 폴더에 필요한 라이브러리 파일을 리눅스 라이브러리에서 복사해 옴으로써 해결 
@@ -26,7 +32,8 @@ env = gym.make("LunarLander-v2", render_mode="human")
         신경망 응용 강화학습
 
         함수 평가
-            incremental MC 방법으로 접근 
+            MC 방법으로 접근 
+            # incremental MC 방법으로 접근 
                 장점 가장 간단한 방법
 
         네트워크 설계
@@ -45,45 +52,102 @@ env = gym.make("LunarLander-v2", render_mode="human")
 
 """
 
+def drive_env_random(env_arg, num_of_frame_arg):
 
-observation, info = env.reset()# 초기 상태에 관한 정보.
+    observation, info = env_arg.reset()# 초기 상태에 관한 정보.
 
-print(observation)
-print(type(observation))
-print(observation.shape)
-
-num_of_frame = 1000
-x_input = np.empty((num_of_frame, 8+1))
-y_output = np.empty((num_of_frame, 1))
-
-
-for i in range(num_of_frame):    
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
-    # reward는 프래임 단위로 주어진다.
-    # 한 프래임의 action에 대하여 하나의 reward를 주는 방식. 
-
-    print(action)
-    print(type(action))
+    print(observation)
+    print(type(observation))
     print(observation.shape)
 
-    x_input[i, 0:8] = observation[:] # 파이썬 인덱싱에 주의할 것. 인덱스가 1:8 이면 끝나는 인덱스가 0번 부터 7번까지
-    x_input[i, 8] = action
-    y_output[i, :] = reward
-    print(i)
-    print(reward)
-    print(type(reward))
-
-    if terminated or truncated:
-        observation, info = env.reset()
+    x_input_rtn = np.empty((num_of_frame_arg, 8+1))
+    y_output_rtn = np.empty((num_of_frame_arg, 1))
 
 
-print(x_input.shape)
-print(y_output.shape)
+    for i in range(num_of_frame_arg):    
+        action = env_arg.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env_arg.step(action)
+        # reward는 프래임 단위로 주어진다.
+        # 한 프래임의 action에 대하여 하나의 reward를 주는 방식. 
 
-print(x_input[99, :])
-print(y_output[99, :])
+        print(action)
+        print(type(action))
+        print(observation.shape)
+
+        x_input_rtn[i, 0:8] = observation[:] # 파이썬 인덱싱에 주의할 것. 인덱스가 1:8 이면 끝나는 인덱스가 0번 부터 7번까지
+        x_input_rtn[i, 8] = action
+        y_output_rtn[i, :] = reward
+        print(i)
+        print(reward)
+        print(type(reward))
+
+        if terminated or truncated: 
+            # 시뮬레이터가 종료되면 재시작이 필요
+            # 조건에 따라 상황이 종료되면, 쓸모있는 데이터를 모을 것으로 기대되는 상황이 더이상 없다고 취급한다.
+            observation, info = env_arg.reset()
 
 
+    print(x_input_rtn.shape)
+    print(y_output_rtn.shape)
 
-env.close()
+    print(x_input_rtn[num_of_frame_arg-1, :])
+    print(y_output_rtn[num_of_frame_arg-1, :])
+
+    return x_input_rtn, y_output_rtn
+
+def create_model():
+    model_rtn = Sequential()
+    
+    model_rtn.add(Dense(128, input_dim=9, activation='sigmoid'))
+    model_rtn.add(Dense(64, activation='sigmoid'))
+    model_rtn.add(Dense(1, activation='sigmoid'))
+    model_rtn.compile(loss='mse', optimizer=Adam())
+    
+    return model_rtn
+
+def learning_model(model_arg, x_input_arg, y_output_arg):
+    model_arg.fit(x_input_arg, y_output_arg, epochs = 5)
+
+def perdict_model(model_arg, observation_arg, action_sapce_arg):
+    # it retruns action values at observation
+    # model_arg : keras model
+    # observation_arg : np.array (1, 8)
+    # action_sapce_arg : np.array (1, 4) 
+    action_space_column_len = action_sapce_arg.shape[0]
+    observation_space_column_len = observation_arg.shape[0]
+
+    action_value_rtn = np.empty((action_space_column_len))
+    model_input = np.empty(observation_space_column_len + action_space_column_len)
+    for i in range(action_space_column_len):#action_space_arg의 열길이에 따라 평가를 반복
+        action = action_sapce_arg[i]
+        print("================")
+        print(observation_arg)
+        print(action)
+        model_input[0:8] = observation_arg  # 0~7번 인덱스
+        model_input[8] = action             #8번 인덱스
+        action_value_rtn[i] = model_arg.predict(model_input)
+
+    return action_value_rtn
+
+if __name__ == '__main__':
+    env = gym.make("LunarLander-v2", render_mode="human")
+
+    num_of_frame = 500
+    x_input, y_output = drive_env_random(env, num_of_frame)
+    
+    # print("sleep...")
+    # time.sleep(5)
+    # x_input, y_output = drive_env_random(env, num_of_frame)
+
+    model_glb = create_model()
+    learning_model(model_glb, x_input, y_output)
+    
+    # test_obs = np.array([ 0.84823453  0.26283216  1.11286175 -1.18096614 -0.27859148 -0.07228909,  0.          0.          0.        ])
+    test_obs = np.array([0.84823453, 0.26283216, 1.11286175, -1.18096614, -0.27859148, -0.07228909, 0, 0])
+
+    action_space = np.array([0, 1, 2, 3])
+    print(action_space.shape)
+    action_value = perdict_model(model_glb, test_obs, action_space)
+    print(action_value)
+
+    env.close()
