1e7646ceff6c0cf17a9a1b090789efdd23e581e1
sangwoo_kim <kimz1121@naver.com>
2023-05-07 20:57:01 +0900

    0507 ai test 02

--- cliff_walking/cliff_walking.py
+++ cliff_walking/cliff_walking.py
@@ -1,5 +1,4 @@
 import time
-import keyboard
 
 import gymnasium as gym
 import matplotlib.pyplot as plt
@@ -10,12 +9,12 @@ def env_drive_by_key(env_arg):
     env.reset()
     while (1):
         print("==================")
-        key_input = get_keyboard_input
+        key_input = get_keyboard_input()
         print(key_input) 
         if key_input == 4:
             break
 
-        observation, reward, terminated, truncated, info = env_arg.step()
+        observation, reward, terminated, truncated, info = env_arg.step(key_input)
         print(observation)
         print(reward)
         print(terminated)
@@ -24,29 +23,38 @@ def env_drive_by_key(env_arg):
         if terminated == 1:
             break
 
-
-
 def get_keyboard_input():
     key_input = -1
-    while 1:
-        if keyboard.is_pressed("up") == 1:
-            key_input = 0
-        if keyboard.is_pressed("right") == 1:
-            key_input = 1
-        if keyboard.is_pressed("down") == 1:
-            key_input = 2
-        if keyboard.is_pressed("left") == 1:
-            key_input = 3
+    # while 1:
+    #     if keyboard.is_pressed("up") == 1:
+    #         key_input = 0
+    #     if keyboard.is_pressed("right") == 1:
+    #         key_input = 1
+    #     if keyboard.is_pressed("down") == 1:
+    #         key_input = 2
+    #     if keyboard.is_pressed("left") == 1:
+    #         key_input = 3
 
-        if keyboard.is_pressed("x") == 1:
-            key_input = 4
-        if keyboard.is_pressed("X") == 1:
-            key_input = 4
-        if keyboard.is_pressed("esc") == 1:
-            key_input = 4
+    #     if keyboard.is_pressed("x") == 1:
+    #         key_input = 4
+    #     if keyboard.is_pressed("X") == 1:
+    #         key_input = 4
+    #     if keyboard.is_pressed("esc") == 1:
+    #         key_input = 4
 
-        if key_input != -1: 
-            break
+        # if key_input != -1: 
+        #     break
+
+    key_value = input()
+    if key_value == "1":
+        key_input = 0
+    elif key_value == "4":
+        key_input = 1
+    elif key_value == "2":
+        key_input = 2
+    elif key_value == "3":
+        key_input = 3
+
 
     return key_input
 
